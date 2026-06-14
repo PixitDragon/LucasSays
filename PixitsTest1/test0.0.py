94d53364c99b9654b08822872c0618c943175252
unknown <rianaddy@gmail.com>
2026-06-13 22:21:10 -0400

    move test to main functions

--- PixitsTest1/test0.0.py
+++ PixitsTest1/test0.0.py
@@ -5,16 +5,6 @@ from pydub.playback import play
 # 1. Get the directory of the current script
 script_dir = str(Path(__file__).parent)
 
-goingPath=script_dir+"/testSounds/going.mp3"
-
-testSound=AudioSegment.from_file("/Users/pixitt/Documents/GitHub/LucasSays/PixitsTest1/test.mp3", format="mp3")
-goingSound=AudioSegment.from_file(goingPath, format="mp3")
-soundSound=AudioSegment.from_file("/Users/pixitt/Documents/GitHub/LucasSays/PixitsTest1/testSounds/sound.mp3", format="mp3")
-
-combined = testSound + goingSound + soundSound
-
-# simple export
-file_handle = combined.export("./output2.mp3", format="mp3")
 
 
 
@@ -38,12 +28,12 @@ for word in sentence:
     curSound=AudioSegment.from_file(curPath, format="mp3")
     combined2+=curSound
 
-curPath=basePath+"lucasOutput3.mp3"
-file_handle = combined2.export(curPath, format="mp3")
+outputPath=basePath+"lucasOutput3.mp3"
+file_handle = combined2.export(outputPath, format="mp3")
 
 
 # Load your audio file (works for MP3, WAV, etc.)
-sound = AudioSegment.from_file(curPath, format="mp3")
+sound = AudioSegment.from_file(outputPath, format="mp3")
 
 # Play the audio file out loud
 play(sound)
--- main.py
+++ main.py
@@ -1,17 +1,41 @@
+from pydub import AudioSegment 
+from pathlib import Path
+from pydub.playback import play
+
 #gather user input and return files / errors for missing words / also handle future setings logic (skip unkwnow words instead of error ect)
 wordList = {
-            "test": "test",
-            "time": "time",
-            "furries": "furries.mp3",
-            "pixit": "pixit.mp3",
+            "_space": "_space",
+            "as": "as",
+            "first": "first",
+            "hell": "hell",
+            "i'm": "i'm",
+            "let's": "let's",
+            "reason": "reason",
+            "same": "same",
+            "so": "so",
+            "started": "started",
+            "then": "then",
+            "things": "things",
+            "tired": "tired",
+            "uh": "uh",
+            "well": "well"
             }
+
+
+def wordPath(word):
+    script_dir = str(Path(__file__).parent)
+    basePath=script_dir+"/LucasSounds/"
+    endingPath=".mp3"
+
+    curPath=basePath+"_space"+endingPath
+    return(curPath)
 
 
 def userInput():
     print('What do you want lucas to say')
     userText = input()
     userInput = userText.split()
-    processText(userInput,wordList)
+    processAudio(userInput,wordList)
 
 def processText(userInput, wordList):
     for x in userInput:
@@ -20,4 +44,20 @@ def processText(userInput, wordList):
         else:
             print("Nope")
 
+def processAudio(userInput,wordList):
+    outputSound = AudioSegment.from_file(wordPath("_space"), format="mp3")
+    for word in userInput:
+        if word in wordList:
+            outputSound += AudioSegment.from_file(wordPath(word), format="mp3")
+        else:
+            raise ValueError("Word not in dictionary")
+    return(outputSound)
+
+def exportFile(soundOutput):
+    script_dir = str(Path(__file__).parent)
+
+    outputPath=script_dir+"outputs/"+"lucasOutput3.mp3"
+    soundOutput.export(outputPath, format="mp3")
+
 userInput()
+#C:\Users\Rain\Desktop\personal proj\LucasSaysProject\LucasSounds\_space.mp3
