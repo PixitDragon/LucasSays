a1de8595f1020611274bf5529d02b5f6492ce788
unknown <rianaddy@gmail.com>
2026-06-18 19:48:46 -0400

    file collision prevention

--- mainER.py
+++ mainER.py
@@ -2,6 +2,7 @@ from pydub import AudioSegment
 from pathlib import Path
 from pydub.playback import play
 
+
 def userInput():
     print('What do you want lucas to say')
     userText = input()
@@ -12,14 +13,32 @@ def getWordPath(word):
     basepath = str(Path(__file__).parent)
     folderPath = basepath + "/LucasSounds/"
     endingPath = ".mp3"
-    return(folderPath + word + endingPath)
+    outputPath = folderPath + word + endingPath
+    outputPathPath = Path(outputPath)
+    if outputPathPath.is_file():
+        return(outputPath)
+    else:
+        print("we do not have the word " + word)
+        
 
 def getAudioSegment(word):
     return(AudioSegment.from_file(getWordPath(word), format="mp3"))
 
 def exportPath(exportName):
     basepath = str(Path(__file__).parent)
-    return(basepath + "/outputs/" + exportName + ".mp3")
+    exportPath = basepath + "/outputs/" + exportName + ".mp3"
+
+    exportPathPath = Path(exportPath)
+    if exportPathPath.is_file():
+        print("name already exists chose again -_-")
+        while exportPathPath.is_file():
+            print("new file name please")
+            exportName = input()
+            exportPath = basepath + "/outputs/" + exportName + ".mp3"
+            exportPathPath = Path(exportPath)
+        return(exportPath)
+    else:
+        return(exportPath)
 
 def exportThatStuff(fullAudio):
     print("what should the file be called?")
